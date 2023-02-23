ae8e82bd3dcc3f7eccd687db8345c5646bd17737
mahedihasanstat <35622406+mahedihasanstat@users.noreply.github.com>
2023-02-23 00:56:13 -0800

    Update Visualization.R

---
+++ Visualization.R
@@ -0,0 +1,14 @@
+## Installing the required package and get the dataset
+library(ISLR)
+
+## Let see what are dataset available in ISLR package
+data(package = "ISLR")
+
+## Calling a dataset into R
+data("Credit")
+
+## To see the list of the variables in the dataset
+names(Credit)
+
+## To see the structure of the variables
+str(Credit)
