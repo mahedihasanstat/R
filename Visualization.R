c5855b1764915a806e1da423cd5762d75dd19a55
mahedihasanstat <35622406+mahedihasanstat@users.noreply.github.com>
2023-02-23 00:59:45 -0800

    Update Visualization.R

--- Visualization.R
+++ Visualization.R
@@ -12,3 +12,33 @@ names(Credit)
 
 ## To see the structure of the variables
 str(Credit)
+
+
+#########################################
+############## Basic Plots###############
+# Histogram
+hist(Credit$Age, breaks = 4)
+
+# Bar Chart
+counts = table(Credit$Ethnicity)
+barplot(counts, main = "Ethnicity Distribution",
+        xlab = "Ethnicities", ylab = "Frequency" )
+
+
+# Density Plot
+d = density(Credit$Income)
+plot(d, col= "red", main = "Income Distribution", 
+                    xlab = "Income", ylab = "Probability")
+
+
+# Scatter Plot
+plot(Credit$Income, Credit$Balance, main = "Scatter Plot between income and Balance", 
+            xlab = " Income", ylab = "Balance")
+
+
+
+
+
+
+
+
