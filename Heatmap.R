0feb1a1192dc05018968b435bdc51194607c0a43
mahedihasanstat <35622406+mahedihasanstat@users.noreply.github.com>
2023-02-24 00:01:02 -0800

    Update Heatmap.R

---
+++ Heatmap.R
@@ -0,0 +1,31 @@
+
+
+library(ISLR)
+data("Credit")
+names(Credit)
+str(Credit)
+
+data.cor = Credit[, 2:7]
+names(mydata)
+
+cor.data = cor(data.cor)
+cor.data
+
+library(ggplot2)
+library(reshape2)
+
+data.melt = melt(cor.data)
+data.melt
+
+ggplot(data.melt, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() + scale_fill_gradient(high = "blue", low = "white")
+
+
+
+
+ggplot(data.melt, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile() + scale_fill_distiller(palette = "Spectral")
+
+
+
+
